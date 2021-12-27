ac03dca04f53d1c2681eda8976c7ef512af8bfa2
Maria Alberdina <mariyaalberdina1980@gmail.com>
2021-12-27 13:09:34 +0400

    my 6 commit from website creation

--- basicchem/models.py
+++ basicchem/models.py
@@ -17,6 +17,9 @@ class Basicchem(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        ordering = ['title']
+
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
 
